bee4b5135dd3dd5fec4cfe353e4b1dc3113a70f3
Youssef Lahrour <lahrouryoussef2@gmail.com>
2026-01-12 10:35:49 +0100

    Initialisation du projet Boucherie Agadir avec configuration sécurisée

---
+++ db.exemple.php
@@ -0,0 +1,14 @@
+<?php
+// Modèle de connexion pour la Boucherie Agadir
+// Renommez ce fichier en db.php et complétez les infos
+$host = 'localhost';
+$dbname = 'boucherie_agadir';
+$user = 'votre_utilisateur';
+$pass = 'votre_mot_de_passe';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+?>
