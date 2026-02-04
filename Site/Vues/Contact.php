697f2bb77c0a5e98e6abdf8e5913e47a45da5d00
Youssef Lahrour <130902370+LahrourYoussef@users.noreply.github.com>
2026-02-04 18:44:21 +0000

    ne pas faire attention

--- Site/Vues/Contact.php
+++ Site/Vues/Contact.php
@@ -6,7 +6,6 @@
     <meta name="description" content="Boucherie Agadir - Viande fraîche et de qualité depuis 2022. Découvrez notre sélection de viandes préparées avec soin." />
     <title>Contact</title>
     <link rel="stylesheet" href="../Styles/style.css" />
-    <link rel="stylesheet" href="../Styles/Contact.css" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 </head>
 <body>
