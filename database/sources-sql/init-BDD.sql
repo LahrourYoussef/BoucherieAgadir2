c309a596a0b5b6f8007f1abf7f7f52f448191963
Youssef Lahrour <lahrouryoussef2@gmail.com>
2026-02-17 10:33:26 +0100

    oooooooo

--- database/sources-sql/init-BDD.sql
+++ database/sources-sql/init-BDD.sql
@@ -1,20 +1,23 @@
 -- script d'initialisation de la base de données pour l'application web avec les droits de l'utilisateur
-DROP DATABASE IF EXISTS `tp_sio2_bdjourneeintegration`;
+DROP DATABASE IF EXISTS `boucherie`;
 
-CREATE DATABASE IF NOT EXISTS `tp_sio2_bdjourneeintegration` CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `boucherie` CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE DATABASE IF NOT EXISTS `tp_sio2_bdjourneeintegration` CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `boucherie` CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 DROP USER IF EXISTS 'JI_Dev_Read'@'%';
 
 CREATE USER 'JI_Dev_Read'@'%' IDENTIFIED BY 'pwdJIPourDev_R';
 
 
-USE `tp_sio2_bdjourneeintegration`;
+USE `boucherie`;
 
 DROP USER IF EXISTS 'Agadir'@'%';
 
 CREATE USER 'Agadir'@'%' IDENTIFIED BY 'Agadir';
+
+GRANT ALL PRIVILEGES ON boucherie.* TO 'Agadir'@'%';
+FLUSH PRIVILEGES;
 
 DROP USER IF EXISTS 'SIO2'@'%';
 
